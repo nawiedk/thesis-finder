8b75b4296156200e0e1cdcbccfb04b6452ac5287
Nawied Khaleqi <74689002+nawiedk@users.noreply.github.com>
2026-01-19 23:54:54 +0100

    Add Flyway clean migration strategy and update schema definitions

--- src/main/java/com/devsxplore/thesis/ThesisApplication.java
+++ src/main/java/com/devsxplore/thesis/ThesisApplication.java
@@ -2,12 +2,22 @@ package com.devsxplore.thesis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.flyway.autoconfigure.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ThesisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ThesisApplication.class, args);
+    }
+
+    @Bean
+    public FlywayMigrationStrategy cleanMigrationStrategy() {
+        return flyway -> {
+            flyway.clean();
+            flyway.migrate();
+        };
     }
 
 }
