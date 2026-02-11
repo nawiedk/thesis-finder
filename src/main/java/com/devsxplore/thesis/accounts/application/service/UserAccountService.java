5655e5cc1158b79e776b80285506055ed7284010
Nawied Khaleqi <74689002+nawiedk@users.noreply.github.com>
2026-02-11 20:43:38 +0100

    Add UserAccountService

---
+++ src/main/java/com/devsxplore/thesis/accounts/application/service/UserAccountService.java
@@ -0,0 +1,62 @@
+package com.devsxplore.thesis.accounts.application.service;
+
+import com.devsxplore.thesis.accounts.application.port.in.command.AssignUserRoleCommand;
+import com.devsxplore.thesis.accounts.application.port.in.command.LoadAccountCommand;
+import com.devsxplore.thesis.accounts.application.port.in.command.RegisterAccountCommand;
+import com.devsxplore.thesis.accounts.application.port.in.usecase.AssignUserRoleUseCase;
+import com.devsxplore.thesis.accounts.application.port.in.usecase.ListAccountsUseCase;
+import com.devsxplore.thesis.accounts.application.port.in.usecase.LoadAccountUseCase;
+import com.devsxplore.thesis.accounts.application.port.in.usecase.RegisterOrUpdateAccountUseCase;
+import com.devsxplore.thesis.accounts.application.port.out.UserAccountRepositoryPort;
+import com.devsxplore.thesis.accounts.domain.model.UserAccount;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.devsxplore.thesis.accounts.domain.model.UserAccount.createUserAccountWithId;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserAccountService implements
+        AssignUserRoleUseCase,
+        ListAccountsUseCase,
+        LoadAccountUseCase,
+        RegisterOrUpdateAccountUseCase {
+
+
+    private final UserAccountRepositoryPort userAccountRepositoryPort;
+
+    @Override
+    public void assignRole(AssignUserRoleCommand command) {
+        UserAccount account = userAccountRepositoryPort.findById(command.githubId())
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+        account.assignRole(command.role());
+        userAccountRepositoryPort.save(account);
+    }
+
+    @Override
+    public List<UserAccount> loadAll() {
+        return userAccountRepositoryPort.findAll();
+    }
+
+    @Override
+    public UserAccount loadById(LoadAccountCommand command) {
+        return userAccountRepositoryPort.findById(command.githubId()).orElseThrow(() -> new IllegalArgumentException("User not found"));
+    }
+
+    @Override
+    public UserAccount registerOrUpdate(RegisterAccountCommand command) {
+        String displayName = command.displayName() == null ? command.login() : command.displayName();
+        UserAccount account = userAccountRepositoryPort.findById(command.githubId())
+                .orElseGet(() -> createUserAccountWithId(
+                        command.githubId(),
+                        command.login(),
+                        displayName
+                ));
+        account.updateProfile(command.login(), displayName);
+        return userAccountRepositoryPort.save(account);
+    }
+}
