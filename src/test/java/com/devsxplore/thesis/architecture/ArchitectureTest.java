004320a303780c56f564e08c676e594138d1a77d
Nawied Khaleqi <74689002+nawiedk@users.noreply.github.com>
2026-02-16 20:33:20 +0100

    Add architecture tests to enforce onion architecture and dependency rules

---
+++ src/test/java/com/devsxplore/thesis/architecture/ArchitectureTest.java
@@ -0,0 +1,35 @@
+package com.devsxplore.thesis.architecture;
+
+import com.devsxplore.thesis.ThesisApplication;
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import org.springframework.data.repository.Repository;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+import static com.tngtech.archunit.library.Architectures.onionArchitecture;
+
+@AnalyzeClasses(packagesOf = ThesisApplication.class, importOptions = {ImportOption.DoNotIncludeTests.class})
+public class ArchitectureTest {
+
+    @ArchTest
+    static final ArchRule onion_architecture_is_respected = onionArchitecture()
+            .domainModels("..domain.model..")
+            .domainServices("..domain.service..")
+            .applicationServices("..application..")
+            .adapter("web", "..adapter.in.web..")
+            .adapter("persistence", "..adapter.out.persistence..");
+
+    @ArchTest
+    static final ArchRule ports_should_not_depend_on_spring_data = classes()
+            .that().resideInAPackage("..application.port..")
+            .should().notBeAssignableTo(Repository.class)
+            .because("Ports in der Application-Schicht sollten Framework-agnostisch sein.");
+
+    @ArchTest
+    static final ArchRule domain_should_not_depend_on_application = noClasses()
+            .that().resideInAPackage("..domain..")
+            .should().dependOnClassesThat().resideInAPackage("..application..");
+}
