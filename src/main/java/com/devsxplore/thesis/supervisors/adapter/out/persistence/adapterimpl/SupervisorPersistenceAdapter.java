a73a590d5e428506d2091cd2ab5dbb2635abbb29
Nawied Khaleqi <74689002+nawiedk@users.noreply.github.com>
2026-02-16 23:14:57 +0100

    Refactor SupervisorPersistenceAdapter and SupervisorRepository: update methods to use SupervisorMapper, add userId-based loading and deletion, and enhance repository interface

--- src/main/java/com/devsxplore/thesis/supervisors/adapter/out/persistence/adapterimpl/SupervisorPersistenceAdapter.java
+++ src/main/java/com/devsxplore/thesis/supervisors/adapter/out/persistence/adapterimpl/SupervisorPersistenceAdapter.java
@@ -1,7 +1,7 @@
 package com.devsxplore.thesis.supervisors.adapter.out.persistence.adapterimpl;
 
-import com.devsxplore.thesis.supervisors.adapter.out.persistence.mapper.SupervisorMapper;
 import com.devsxplore.thesis.supervisors.adapter.out.persistence.jdbcentity.SupervisorJDBCEntity;
+import com.devsxplore.thesis.supervisors.adapter.out.persistence.mapper.SupervisorMapper;
 import com.devsxplore.thesis.supervisors.adapter.out.persistence.repository.SupervisorRepository;
 import com.devsxplore.thesis.supervisors.application.port.out.SupervisorRepositoryPort;
 import com.devsxplore.thesis.supervisors.domain.model.Supervisor;
@@ -11,42 +11,51 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.Optional;
 
-import static com.devsxplore.thesis.supervisors.adapter.out.persistence.mapper.SupervisorMapper.mapSupervisorToDomainEntity;
-import static com.devsxplore.thesis.supervisors.adapter.out.persistence.mapper.SupervisorMapper.mapSupervisorToJDBCEntity;
-
 @Component
 @RequiredArgsConstructor
 public class SupervisorPersistenceAdapter implements SupervisorRepositoryPort {
 
     private final SupervisorRepository supervisorRepository;
+    private final SupervisorMapper supervisorMapper;
 
     @Override
     public Supervisor save(Supervisor supervisor) {
-        var entity = mapSupervisorToJDBCEntity(supervisor);
+        SupervisorJDBCEntity entity = supervisorMapper.mapSupervisorToJDBCEntity(supervisor);
         SupervisorJDBCEntity newSupervisor = supervisorRepository.save(entity);
-        return mapSupervisorToDomainEntity(newSupervisor);
+        return supervisorMapper.mapSupervisorToDomainEntity(newSupervisor);
     }
 
     @Override
-    public Optional<Supervisor> load(Long id) {
-        return supervisorRepository.findById(id)
-                .map(SupervisorMapper::mapSupervisorToDomainEntity);
+    public Optional<Supervisor> load(Long supervisorId) {
+        return supervisorRepository.findByUserId(supervisorId)
+                .map(supervisorMapper::mapSupervisorToDomainEntity);
     }
 
     @Override
     public List<Supervisor> loadAll() {
         return supervisorRepository.findAll()
                 .stream()
-                .map(SupervisorMapper::mapSupervisorToDomainEntity)
+                .map(supervisorMapper::mapSupervisorToDomainEntity)
                 .toList();
     }
 
     @Override
     public boolean delete(Long supervisorId) {
-        if (supervisorRepository.existsById(supervisorId)) {
-            supervisorRepository.deleteById(supervisorId);
+        if (supervisorRepository.existsByUserId(supervisorId)) {
+            supervisorRepository.deleteBySupervisorId(supervisorId);
             return true;
         }
         return false;
+    }
+
+    @Override
+    public boolean existsBySupervisorUserId(Long supervisorUserId) {
+        return supervisorRepository.existsByUserId(supervisorUserId);
+    }
+
+    @Override
+    public Optional<Supervisor> loadByUserId(Long userId) {
+        return supervisorRepository.findByUserId(userId)
+                .map(supervisorMapper::mapSupervisorToDomainEntity);
     }
 }
--- src/main/java/com/devsxplore/thesis/supervisors/adapter/out/persistence/repository/SupervisorRepository.java
+++ src/main/java/com/devsxplore/thesis/supervisors/adapter/out/persistence/repository/SupervisorRepository.java
@@ -5,9 +5,16 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SupervisorRepository extends CrudRepository<SupervisorJDBCEntity, Long> {
     @Override
     List<SupervisorJDBCEntity> findAll();
+
+    Optional<SupervisorJDBCEntity> findByUserId(Long userId);
+
+    boolean existsByUserId(Long userId);
+
+    void deleteBySupervisorId(Long supervisorId);
 }
