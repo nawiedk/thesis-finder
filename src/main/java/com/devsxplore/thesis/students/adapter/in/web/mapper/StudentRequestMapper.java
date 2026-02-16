0a9261f6cd24b29dcb38bff8ce0b3b99571802c6
Nawied Khaleqi <74689002+nawiedk@users.noreply.github.com>
2026-02-16 16:29:38 +0100

    Add StudentRequestMapper and StudentResponseMapper for handling student data transformations

---
+++ src/main/java/com/devsxplore/thesis/students/adapter/in/web/mapper/StudentRequestMapper.java
@@ -0,0 +1,32 @@
+package com.devsxplore.thesis.students.adapter.in.web.mapper;
+
+import com.devsxplore.thesis.students.adapter.in.web.dto.request.ChangeCourseDTO;
+import com.devsxplore.thesis.students.adapter.in.web.dto.request.ChangeInterestDTO;
+import com.devsxplore.thesis.students.adapter.in.web.dto.request.RegisterStudentDTO;
+import com.devsxplore.thesis.students.adapter.in.web.dto.request.UpdateStudentProfileDTO;
+import com.devsxplore.thesis.students.application.port.in.command.*;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentRequestMapper {
+
+    public RegisterStudentCommand mapToRegisterStudentCommand(Long studentUserId, RegisterStudentDTO dto) {
+        return new RegisterStudentCommand(studentUserId, dto.firstName(), dto.lastName());
+    }
+
+    public LoadStudentByUserIdCommand mapToLoadStudentByUserIdCommand(Long studenUserId) {
+        return new LoadStudentByUserIdCommand(studenUserId);
+    }
+
+    public UpdateStudentProfileCommand mapToUpdateStudentProfileCommand(Long studentUserId, UpdateStudentProfileDTO dto) {
+        return new UpdateStudentProfileCommand(studentUserId, dto.firstName(), dto.lastName());
+    }
+
+    public ChangeCourseCommand mapToChangeCourseCommand(Long studentUserId, ChangeCourseDTO dto) {
+        return new ChangeCourseCommand(studentUserId, dto.course());
+    }
+
+    public ChangeInterestCommand mapToChangeInterestCommand(Long studentUserId, ChangeInterestDTO dto) {
+        return new ChangeInterestCommand(studentUserId, dto.interest());
+    }
+}
---
+++ src/main/java/com/devsxplore/thesis/students/adapter/in/web/mapper/StudentResponseMapper.java
@@ -0,0 +1,39 @@
+package com.devsxplore.thesis.students.adapter.in.web.mapper;
+
+import com.devsxplore.thesis.students.adapter.in.web.dto.response.UserProfileDTO;
+import com.devsxplore.thesis.students.domain.model.Course;
+import com.devsxplore.thesis.students.domain.model.Interest;
+import com.devsxplore.thesis.students.domain.model.Student;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+public class StudentResponseMapper {
+
+    public UserProfileDTO mapToUserProfileDTO(Student student) {
+        Set<String> courses = new HashSet<>();
+        Set<String> interests = new HashSet<>();
+
+        if (!student.getCourses().isEmpty())
+            courses = student.getCourses()
+                    .stream()
+                    .map(Course::course)
+                    .collect(Collectors.toSet());
+        if (!student.getInterests().isEmpty())
+            interests = student.getInterests()
+                    .stream()
+                    .map(Interest::interest)
+                    .collect(Collectors.toSet());
+
+        return new UserProfileDTO(
+                student.getFirstName(),
+                student.getLastName(),
+                student.getFullName(),
+                courses,
+                interests
+        );
+    }
+}
